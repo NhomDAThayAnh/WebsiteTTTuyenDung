e2ddf4939c08a8e15221933fb31371508037a6cb
minhdoan96 <minhdoan.zaphkiel@gmail.com>
2017-12-27 00:01:11 +0700

    Don dang ky

--- WebSiteTinTuc/WebSiteTinTuc/Controllers/HomeController.cs
+++ WebSiteTinTuc/WebSiteTinTuc/Controllers/HomeController.cs
@@ -8,9 +8,61 @@ namespace WebSiteTinTuc.Controllers
 {
     public class HomeController : Controller
     {
+         TUYENDUNGITEntities6 td = new TUYENDUNGITEntities6();
+      
         public ActionResult TrangChu() {
-            return View();
+            var tt = td.TinTucTuyenDung.ToList();
+            ViewBag.CanhBao = "Bạn phải đăng nhập mới có thể nộp đơn";
+            return View(tt);
         }
+
+        [HttpGet]
+        public ActionResult TrangNopDon(int IDTT) {
+            var tttd = td.TinTucTuyenDung.SingleOrDefault(n => n.IDTT == IDTT);
+            Session["IDTD"] = IDTT;
+
+            return View(tttd);
+        }
+
+        [HttpPost]
+        public ActionResult TrangNopDon(TinTucTuyenDung tttd, FormCollection f)
+        {
+            string sNguoiNop = f.Get("name").ToString();
+            string sEmail = f.Get("email").ToString();
+            string sSDT = f.Get("sdt").ToString();
+            string sGT = f.Get("message").ToString();
+
+            Session["sNguoiNop"] = sNguoiNop;
+            Session["sEmail"] = sEmail;
+            Session["sSDT"] = sSDT;
+            Session["sGT"] = sGT;
+           int id = int.Parse(Session["IDTD"].ToString());
+             tttd = td.TinTucTuyenDung.SingleOrDefault(n => n.IDTT ==id);
+            var nd = new NOPDON();
        
+                nd.IDTT = tttd.IDTT;
+                nd.CongTy = tttd.TenCT;
+                nd.NgayNop = DateTime.Now;
+                nd.NguoiNop = Session["sNguoiNop"].ToString();
+                nd.Hanchot = tttd.Thoihan;
+                nd.Yeucau = tttd.Yeucau;
+                nd.Vitri = tttd.Vitri;
+                nd.Email = Session["sEmail"].ToString();
+                nd.SDT = Session["sSDT"].ToString();
+                nd.Gioithieubanthan = sGT;
+                td.NOPDON.Add(nd);
+                td.SaveChanges();
+            
+            return RedirectToAction("TrangChu", "Home");
+        }
+        public ActionResult DonNguoiDung() {
+            string ht = Session["Tenkhachhang"].ToString();
+            var ds = td.NOPDON.Where(x=>x.NguoiNop== ht).ToList();
+            if (ds.Count == 0) {
+                ViewBag.ThongBao = "Bạn không có nào";
+            }
+            ViewBag.ThongBao = "Bạn có " + ds.Count() + " đơn";
+            return View(ds);
+        }
     }
 }
