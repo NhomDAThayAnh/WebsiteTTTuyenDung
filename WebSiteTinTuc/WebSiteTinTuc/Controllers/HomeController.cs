a7226de59d979d6ae815c7096f70fd13b644dbc6
minhdoan96 <minhdoan.zaphkiel@gmail.com>
2017-12-27 00:15:13 +0700

    warning alert

--- WebSiteTinTuc/WebSiteTinTuc/Controllers/HomeController.cs
+++ WebSiteTinTuc/WebSiteTinTuc/Controllers/HomeController.cs
@@ -16,53 +16,6 @@ namespace WebSiteTinTuc.Controllers
             return View(tt);
         }
 
-        [HttpGet]
-        public ActionResult TrangNopDon(int IDTT) {
-            var tttd = td.TinTucTuyenDung.SingleOrDefault(n => n.IDTT == IDTT);
-            Session["IDTD"] = IDTT;
-
-            return View(tttd);
-        }
-
-        [HttpPost]
-        public ActionResult TrangNopDon(TinTucTuyenDung tttd, FormCollection f)
-        {
-            string sNguoiNop = f.Get("name").ToString();
-            string sEmail = f.Get("email").ToString();
-            string sSDT = f.Get("sdt").ToString();
-            string sGT = f.Get("message").ToString();
-
-            Session["sNguoiNop"] = sNguoiNop;
-            Session["sEmail"] = sEmail;
-            Session["sSDT"] = sSDT;
-            Session["sGT"] = sGT;
-           int id = int.Parse(Session["IDTD"].ToString());
-             tttd = td.TinTucTuyenDung.SingleOrDefault(n => n.IDTT ==id);
-            var nd = new NOPDON();
-       
-                nd.IDTT = tttd.IDTT;
-                nd.CongTy = tttd.TenCT;
-                nd.NgayNop = DateTime.Now;
-                nd.NguoiNop = Session["sNguoiNop"].ToString();
-                nd.Hanchot = tttd.Thoihan;
-                nd.Yeucau = tttd.Yeucau;
-                nd.Vitri = tttd.Vitri;
-                nd.Email = Session["sEmail"].ToString();
-                nd.SDT = Session["sSDT"].ToString();
-                nd.Gioithieubanthan = sGT;
-                td.NOPDON.Add(nd);
-                td.SaveChanges();
-            
-            return RedirectToAction("TrangChu", "Home");
-        }
-        public ActionResult DonNguoiDung() {
-            string ht = Session["Tenkhachhang"].ToString();
-            var ds = td.NOPDON.Where(x=>x.NguoiNop== ht).ToList();
-            if (ds.Count == 0) {
-                ViewBag.ThongBao = "Bạn không có nào";
-            }
-            ViewBag.ThongBao = "Bạn có " + ds.Count() + " đơn";
-            return View(ds);
-        }
+        
     }
 }
