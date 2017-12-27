ba2440e1c4aea2d4aa1259427d35a2b1ddc58292
CHUNGTHAI\Chung Thai <bachung7596@gmail.com>
2017-12-27 22:35:50 +0700

    update

--- WebSiteTinTuc/WebSiteTinTuc/Controllers/HomeController.cs
+++ WebSiteTinTuc/WebSiteTinTuc/Controllers/HomeController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebSiteTinTuc.Models;
 
 namespace WebSiteTinTuc.Controllers
 {
     public class HomeController : Controller
     {
+        TUYENDUNGITEntities td = new TUYENDUNGITEntities();
         public ActionResult TrangChu() {
             return View();
         }
